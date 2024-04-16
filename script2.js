ed44c0169d1af7205aa7bf3050aa7e66649dbc20
valentimdev <160342688+valentimdev@users.noreply.github.com>
2024-04-16 17:09:06 -0300

    Add files via upload

--- script2.js
+++ script2.js
@@ -4,15 +4,22 @@ var primeira_linha=lines[0].split(' ')
 var segunda_linha=lines[1].split(' ')
 var vencedora1 = [['A1', 'B2', 'C3']]
 console.log(primeira_linha.includes('A1' && 'B2' && 'C2'))
+console.log(primeira_linha.includes('A1'&& 'B2'&& 'C3'))
 console.log(primeira_linha)
-if ((primeira_linha.includes('A1'&& 'B2'&& 'C3')) || (primeira_linha.includes('A1'&& 'A2'&& 'A3')) 
-|| (primeira_linha.includes('B1'&& 'B2'&& 'B3')) || (primeira_linha.includes('C1'&& 'C2'&& 'C3'))
-|| (primeira_linha.includes('A1'&& 'B1'&& 'C1'))|| (primeira_linha.includes('A2'&& 'B2'&& 'C2'))
-|| (primeira_linha.includes('A3'&& 'B3'&& 'C3')) ){
-    console.log('Player 1 Wins!')
-}else if ((segunda_linha.includes('A1'&& 'B2'&& 'C3')) || (segunda_linha.includes('A1'&& 'A2'&& 'A3')) 
-|| (segunda_linha.includes('B1'&& 'B2'&& 'B3')) || (segunda_linha.includes('C1'&& 'C2'&& 'C3'))
-|| (segunda_linha.includes('A1'&& 'B1'&& 'C1'))|| (segunda_linha.includes('A2'&& 'B2'&& 'C2'))
-|| (segunda_linha.includes('A3'&& 'B3'&& 'C3')) ){
+if ((primeira_linha.includes('A1') && primeira_linha.includes('B2') && primeira_linha.includes('C3')) ||
+    (primeira_linha.includes('A1') && primeira_linha.includes('A2') && primeira_linha.includes('A3')) ||
+    (primeira_linha.includes('B1') && primeira_linha.includes('B2') && primeira_linha.includes('B3')) ||
+    (primeira_linha.includes('C1') && primeira_linha.includes('C2') && primeira_linha.includes('C3')) ||
+    (primeira_linha.includes('A1') && primeira_linha.includes('B1') && primeira_linha.includes('C1')) ||
+    (primeira_linha.includes('A2') && primeira_linha.includes('B2') && primeira_linha.includes('C2')) ||
+    (primeira_linha.includes('A3') && primeira_linha.includes('B3') && primeira_linha.includes('C3'))) {
+    console.log('Player 1 Wins!');
+}else if ((segunda_linha.includes('A1') && segunda_linha.includes('B2') && segunda_linha.includes('C3')) ||
+(segunda_linha.includes('A1') && segunda_linha.includes('A2') && segunda_linha.includes('A3')) ||
+(segunda_linha.includes('B1') && segunda_linha.includes('B2') && segunda_linha.includes('B3')) ||
+(segunda_linha.includes('C1') && segunda_linha.includes('C2') && segunda_linha.includes('C3')) ||
+(segunda_linha.includes('A1') && segunda_linha.includes('B1') && segunda_linha.includes('C1')) ||
+(segunda_linha.includes('A2') && segunda_linha.includes('B2') && segunda_linha.includes('C2')) ||
+(segunda_linha.includes('A3') && segunda_linha.includes('B3') && segunda_linha.includes('C3'))){
     console.log('Player 2 Wins!')
 }else{console.log('Draw!')}
