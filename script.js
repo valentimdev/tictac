0be3a78e0cc4480b12f9b8bddb41c9fcef9195fd
valentimdev <160342688+valentimdev@users.noreply.github.com>
2024-04-20 02:07:45 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 var input = require('fs').readFileSync('stdin', 'utf8');
 var lines = input.split('\n');
-var primeira_linha=lines[1].split(' ')
-var segunda_linha=lines[2].split(' ')
-var linhas = ['A', 'B', 'C'];
-var  colunas = ['1', '2', '3'];
-var diagonais = [['A1', 'B2', 'C3'], ['A3', 'B2', 'C1']]
-for(var i=0;i<primeira_linha.length;i++){
-    var combinação=linhas[i]+colunas[i]
-    if(combinação==primeira_linha[i]){
-        
-    }
+var primeira_linha=lines[0].split(' ')
+var segunda_linha=lines[1].split(' ')
+var vencedora1 = [['A1', 'B2', 'C3']]
+console.log(primeira_linha)
+function checar(linha){
+    return (linha.includes('A1') && linha.includes('B2') && linha.includes('C3')) ||
+    (linha.includes('A1') && linha.includes('A2') && linha.includes('A3')) ||
+    (linha.includes('B1') && linha.includes('B2') && linha.includes('B3')) ||
+    (linha.includes('C1') && linha.includes('C2') && linha.includes('C3')) ||
+    (linha.includes('A1') && linha.includes('B1') && linha.includes('C1')) ||
+    (linha.includes('A2') && linha.includes('B2') && linha.includes('C2')) ||
+    (linha.includes('A3') && linha.includes('B3') && linha.includes('C3'))
 }
-if()
+if ((checar(primeira_linha))==true) {
+    console.log('Player 1 Wins!');
+}else if ((checar(segunda_linha))==true){
+    console.log('Player 2 Wins!')
+}else{console.log('Draw!')}
--- script2.js
+++ script2.js
@@ -3,9 +3,16 @@ var lines = input.split('\n');
 var primeira_linha=lines[0].split(' ')
 var segunda_linha=lines[1].split(' ')
 var vencedora1 = [['A1', 'B2', 'C3']]
-console.log(primeira_linha.includes('A1' && 'B2' && 'C2'))
-console.log(primeira_linha.includes('A1'&& 'B2'&& 'C3'))
 console.log(primeira_linha)
+function checar(linha){
+    return (linha.includes('A1') && linha.includes('B2') && linha.includes('C3')) ||
+    (linha.includes('A1') && linha.includes('A2') && linha.includes('A3')) ||
+    (linha.includes('B1') && linha.includes('B2') && linha.includes('B3')) ||
+    (linha.includes('C1') && linha.includes('C2') && linha.includes('C3')) ||
+    (linha.includes('A1') && linha.includes('B1') && linha.includes('C1')) ||
+    (linha.includes('A2') && linha.includes('B2') && linha.includes('C2')) ||
+    (linha.includes('A3') && linha.includes('B3') && linha.includes('C3'))
+}
 if ((primeira_linha.includes('A1') && primeira_linha.includes('B2') && primeira_linha.includes('C3')) ||
     (primeira_linha.includes('A1') && primeira_linha.includes('A2') && primeira_linha.includes('A3')) ||
     (primeira_linha.includes('B1') && primeira_linha.includes('B2') && primeira_linha.includes('B3')) ||
