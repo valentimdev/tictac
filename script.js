4fc927e67296161f58651567beb6aa3fbf4cabde
valentimdev <160342688+valentimdev@users.noreply.github.com>
2024-04-20 02:11:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ var input = require('fs').readFileSync('stdin', 'utf8');
 var lines = input.split('\n');
 var primeira_linha=lines[0].split(' ')
 var segunda_linha=lines[1].split(' ')
-var vencedora1 = [['A1', 'B2', 'C3']]
-console.log(primeira_linha)
 function checar(linha){
     return (linha.includes('A1') && linha.includes('B2') && linha.includes('C3')) ||
     (linha.includes('A1') && linha.includes('A2') && linha.includes('A3')) ||
